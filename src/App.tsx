c373af37d99a0415b7d34516304b26c671265240
Eakz <pilpaff@gmail.com>
2022-02-20 18:46:05 +0200

    chore: data visualization and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Platform, UIManager} from 'react-native';
 import {AppProvider} from './App.provider';
 import {BottomTabsNavigator} from './screens/BottomTabs.navigator';
+import SplashScreen from 'react-native-splash-screen';
+import {LogBox} from 'react-native';
+
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
 
 if (Platform.OS === 'android') {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
@@ -11,6 +17,9 @@ if (Platform.OS === 'android') {
 }
 
 export const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <AppProvider>
       <NavigationContainer>
--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {VictoryPie} from 'victory-native';
+import {useAppContext} from '../App.provider';
+
+type TpieData = {
+  x: string;
+  y: number;
+}[];
 
 export const Anaylytics: React.FC = () => {
+  const {moodList} = useAppContext();
+  const [delayedData, setDelayedData] = useState<TpieData>([]);
+  const [endAngle, setEndAngle] = useState<number>(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const data = moodList.reduce((acc, e) => {
+        const indexOfValue = acc.findIndex(v => v.x === e.mood.emoji);
+        if (acc.length && indexOfValue !== -1) {
+          acc[indexOfValue] = {x: e.mood.emoji, y: acc[indexOfValue].y + 1};
+        } else {
+          acc.push({x: e.mood.emoji, y: 1});
+        }
+        return acc;
+      }, [] as TpieData);
+      setDelayedData(data);
+      setEndAngle(360);
+    }, 1000);
+  }, [moodList]);
+  // console.log({moodList: moodList.map(e => e.mood)});
   return (
-    <View>
-      <Text>Anaylytics</Text>
+    <View style={styles.container}>
+      <VictoryPie
+        data={delayedData}
+        endAngle={endAngle}
+        animate={{
+          duration: 1000,
+          easing: 'circleOut',
+        }}
+        colorScale="qualitative"
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
